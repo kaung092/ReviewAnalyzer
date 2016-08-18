246d85e2ef7bf3a49ac445988b91a64c5acaa6f4
Kaung Htet <htet>
2016-08-18 12:17:33 -0400

    init

---
+++ js/Amazon.js
@@ -0,0 +1,29 @@
+function getComments(){
+
+		let comments = [];
+		comments = $("#cm_cr-review_list .review .review-data .review-text").map(function() {
+				return $(this).text();
+		});
+		alert('Click on Review Analyzer icon to get more insight about the reviews!');
+		console.log(comments);
+		/*
+		$('html').append("<span class='frame-wrapper'>"
+										+"<p>Hello</p>"								
+										+"</span>");
+
+		$('.frame-wrapper').draggable();
+		*/
+
+		if(comments!=[]){
+			console.log('has data');
+		}
+		return comments;
+		
+}
+
+$(document).ready(function(){
+    console.log('Extracting text from comments');
+    let comments = getComments();  
+});
+ 
+
---
+++ js/Yelp.js
@@ -0,0 +1,10 @@
+function getComments(){
+	//alert(document.title);
+}
+
+$(document).ready(function(){
+    console.log('Grabbing text from comments');
+    getComments();  
+});
+ 
+
